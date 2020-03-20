41b9fdd074955a28c20e6604918221c07c57ac64
Flybor <18780106592@163.com>
2020-03-20 14:13:31 +0800

    Initial commit

---
+++ Example/XoFoundation/XOViewController.h
@@ -0,0 +1,13 @@
+//
+//  XOViewController.h
+//  XoFoundation
+//
+//  Created by 510462142@qq.com on 03/20/2020.
+//  Copyright (c) 2020 510462142@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XOViewController : UIViewController
+
+@end
---
+++ Example/XoFoundation/XOViewController.m
@@ -0,0 +1,29 @@
+//
+//  XOViewController.m
+//  XoFoundation
+//
+//  Created by 510462142@qq.com on 03/20/2020.
+//  Copyright (c) 2020 510462142@qq.com. All rights reserved.
+//
+
+#import "XOViewController.h"
+
+@interface XOViewController ()
+
+@end
+
+@implementation XOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
